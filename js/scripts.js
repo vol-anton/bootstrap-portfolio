de8641a560e26f16101dede920c130c50655575b
Anton <easiestgame32@gmail.com>
2023-02-14 13:26:10 +0100

    edited

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume) ******EDITED*******
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
